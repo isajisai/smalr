70e864351931dce0b37fc71ca8f48c1c86c3eaf1
isajisai <silaspsadia@gmail.com>
2017-10-09 01:19:37 -0700

    started huffman

---
+++ exam.c
@@ -0,0 +1,20 @@
+
+/*
+	Project: Test zone
+	File: exam.c
+	Description: tests, tests, tests
+	Written by pongozolin, isajisai (2017)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Dog {
+	Dog *next_pupp;
+	unsigned int num_woofs;
+} Dog;
+
+int main() {
+
+}
