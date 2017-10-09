e1358e6d12a02829fbb207ca15aa491cbb24961f
isajisai <silaspsadia@gmail.com>
2017-10-09 01:37:19 -0700

    learning

--- exam.c
+++ exam.c
@@ -10,11 +10,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Dog {
-	Dog *next_pupp;
+typedef struct dog {
+	struct dog *next_pupp;
 	unsigned int num_woofs;
 } Dog;
 
 int main() {
+	Dog *pupp_zone = (Dog *)calloc(256, sizeof(Dog));
+	pupp_zone[33].num_woofs = 25;
+	pupp_zone[33].next_pupp = &pupp_zone[45];
 
+	pupp_zone[45].num_woofs = 88;
+	pupp_zone[45].next_pupp = &pupp_zone[33];
+
+	Dog this_woofer = pupp_zone[33];
+	printf("%i\n", this_woofer.num_woofs);
+	printf("%i\n", (this_woofer.next_pupp)->num_woofs);
+	printf("%i\n", ((this_woofer.next_pupp)->next_pupp)->num_woofs);
+	printf("%i\n", pupp_zone[50].num_woofs);
+	this_woofer.num_woofs++;
+	printf("%i\n", this_woofer.num_woofs);
 }
